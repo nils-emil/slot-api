f19b14e1edee8e67ecdb77df325745382b5576b3
Nils-Emil Lille <nilsemil.lille@gmail.com>
2021-12-05 13:50:08 +0200

    Init spring base

---
+++ src/main/java/ee/slot/SlotMachine/SlotMachineApplication.java
@@ -0,0 +1,13 @@
+package ee.slot.SlotMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/slot/SlotMachine/SlotMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.slot.SlotMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
